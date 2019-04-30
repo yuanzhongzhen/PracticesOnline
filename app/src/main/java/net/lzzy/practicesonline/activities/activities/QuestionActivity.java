1c0ae140b9e300a8b5e205bfb436390136000c82
袁中震 <1392820131@qq.com>
2019-04-30 18:09:22 +0800

    2019-4-30

--- app/src/main/java/net/lzzy/practicesonline/activities/activities/QuestionActivity.java
+++ app/src/main/java/net/lzzy/practicesonline/activities/activities/QuestionActivity.java
@@ -2,17 +2,39 @@ package net.lzzy.practicesonline.activities.activities;
 
 
 import android.os.Bundle;
+import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import net.lzzy.practicesonline.R;
+import net.lzzy.practicesonline.activities.utils.AppUtils;
 
 public class QuestionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_question);
+        AppUtils.addActivity(this);
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        AppUtils.removeActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppUtils.setRunning(getLocalClassName());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        AppUtils.setStopped(getLocalClassName());
     }
 }
---
+++ app/src/main/java/net/lzzy/practicesonline/activities/frageents/QuestionFragment.java
@@ -0,0 +1,22 @@
+package net.lzzy.practicesonline.activities.frageents;
+
+/**
+ * Created by lzzy_gxy on 2019/4/30.
+ * Description:
+ */
+public class QuestionFragment extends BeseFargment {
+    @Override
+    protected void populate() {
+
+    }
+
+    @Override
+    public int getLayoutRes() {
+        return 0;
+    }
+
+    @Override
+    public void search(String kw) {
+
+    }
+}
